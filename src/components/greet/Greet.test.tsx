a52125984c00c89cd67c0a9a622762af81e953a4
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-09-03 13:13:23 +0800

    add test

---
+++ src/components/greet/Greet.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+
+// TDD
+// 1. greet should render the text hello and if a name is passed into the component
+// it should render hello followed by the name
+
+test("Greet renders correctly", () => {
+  render(<Greet />); // RTL uses method to create a virtual dom passing in Greet component
+  const textElement = screen.getByText(/hello/i);
+  expect(textElement).toBeInTheDocument(); // expect and matcher function
+});
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Alvin" />);
+  const textElement = screen.getByText(/hello Alvin/i);
+  expect(textElement).toBeInTheDocument();
+});
---
+++ src/components/greet/Greet.tsx
@@ -0,0 +1,9 @@
+interface GreetProps {
+  name?: string;
+}
+
+function Greet({ name }: GreetProps) {
+  return <div>Hello {name}</div>;
+}
+
+export default Greet;
